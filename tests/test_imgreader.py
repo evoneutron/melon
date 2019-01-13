6d7d6592ccaf7a0fd6c4ac9d5678e64f1072f317
Roman <roman.jugai@ensighten.com>
2019-01-13 11:16:15 -0800

    corrected tests/resources/images

--- tests/test_imgreader.py
+++ tests/test_imgreader.py
@@ -12,16 +12,16 @@ class ImageReaderTestCase(TestCase):
     def test_result_shape(self):
         reader = ImageReader(self._tests_dir)
         x, y = reader.read()
-        self.assertEqual((5, 3, 255, 255), x.shape)
-        self.assertEqual((5, 5), y.shape)
+        self.assertEqual((6, 3, 255, 255), x.shape)
+        self.assertEqual((6, 5), y.shape)
 
     def test_one_hot_result_shape(self):
         reader = ImageReader(self._tests_dir)
         x, y = reader.read()
-        self.assertEqual((5, 3, 255, 255), x.shape)
-        self.assertEqual((5, 5), y.shape)
+        self.assertEqual((6, 3, 255, 255), x.shape)
+        self.assertEqual((6, 5), y.shape)
 
-    def test_batch_read(self):
+    def test_batch_read_when_no_remainder(self):
         reader = ImageReader(self._tests_dir, {"batch_size": 2})
         self.assertTrue(reader.has_next())
         x, y = reader.read()
@@ -35,9 +35,21 @@ class ImageReaderTestCase(TestCase):
 
         self.assertTrue(reader.has_next())
         x, y = reader.read()
+        self.assertEqual((2, 3, 255, 255), x.shape)
+        self.assertEqual(2, y.shape[0])
+        self.assertFalse(reader.has_next())
+
+    def test_batch_read_when_remainder(self):
+        reader = ImageReader(self._tests_dir, {"batch_size": 5})
+        self.assertTrue(reader.has_next())
+        x, y = reader.read()
+        self.assertEqual((5, 3, 255, 255), x.shape)
+        self.assertEqual(5, y.shape[0])
+
+        self.assertTrue(reader.has_next())
+        x, y = reader.read()
         self.assertEqual((1, 3, 255, 255), x.shape)
         self.assertEqual(1, y.shape[0])
-        self.assertFalse(reader.has_next())
 
     def test_batch_read_ensure_all_files_were_read(self):
         mock_img_arr = numpy.ndarray((3, 255, 255))
@@ -67,20 +79,20 @@ class ImageReaderTestCase(TestCase):
 
     def test_one_hot_result_order(self):
         pixels = self._expected_pixels()
-        reader = ImageReader(self._tests_dir, {"normalize": False})
+        reader = ImageReader(self._tests_dir, {"normalize": False, "num_threads": 1})
         x, y = reader.read()
         for i in range(len(y)):
             label = y[i].tolist().index(1)  # in one-hot-encoding location of 1 should match the label
 
             first_five_pixels = x[i][0][0][:5]
             expected = pixels[label]
-            if label == 2:
+            if label == 1:
                 self.assertTrue(numpy.array_equal(expected[0], first_five_pixels) or numpy.array_equiv(expected[1], first_five_pixels))
                 continue
             self.assertTrue(numpy.array_equal(expected, first_five_pixels))
 
-        self.assertEqual((5, 3, 255, 255), x.shape)
-        self.assertEqual((5, 5), y.shape)
+        self.assertEqual((6, 3, 255, 255), x.shape)
+        self.assertEqual((6, 5), y.shape)
 
     def test_one_hot_result_order_when_batch_read(self):
         pixels = self._expected_pixels()
@@ -90,7 +102,7 @@ class ImageReaderTestCase(TestCase):
             label = y[0].tolist().index(1)  # in one-hot-encoding location of 1 should match the label
             first_five_pixels = x[0][0][0][:5]
             expected = pixels[label]
-            if label == 2:
+            if label == 1:
                 self.assertTrue(numpy.array_equal(expected[0], first_five_pixels) or numpy.array_equiv(expected[1], first_five_pixels))
                 continue
             self.assertTrue(numpy.array_equal(expected, first_five_pixels))
@@ -100,10 +112,11 @@ class ImageReaderTestCase(TestCase):
     @staticmethod
     def _expected_pixels():
         pixels = dict()
-        pixels[1] = [0, 0, 0, 0, 0]
-        pixels[2] = [[35, 31, 19, 32, 27], [250, 250, 249, 250, 250]]
-        pixels[3] = [38, 33, 32, 39, 58]
-        pixels[4] = [255, 255, 255, 255, 255]
+        pixels[0] = [0, 0, 0, 0, 0]
+        pixels[1] = [[35, 31, 19, 32, 27], [250, 250, 249, 250, 250]]
+        pixels[2] = [38, 33, 32, 39, 58]
+        pixels[3] = [255, 255, 255, 255, 255]
+        pixels[4] = [246, 246, 246, 246, 246]
         return pixels
 
 
